6c3de46d4035ea1b4962143bdb5e99e4e8e9fe0e
demius_delph <demius_delph@hotmail.com>
2021-10-29 07:44:09 +0300

    edited

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -1,3 +1,21 @@
 let a = 5;
 
 console.log(a);
+
+
+const b = {
+    name: "Dima",
+    year: 1992,
+    proffesion: "web developer"
+};
+
+console.log(b);
+
+let c = (a,b) => {
+    console.log('1');
+    return a+b;
+};
+
+// c(10,15);
+
+// console.log(c);
